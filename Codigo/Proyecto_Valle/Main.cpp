9a01a024bc731a1567cb18bfc1ffd72e4e091b8d
JoGEHrt <espinodehorta99@gmail,com>
2022-05-03 09:40:35 -0500

    Documentacion y prueba de los casos facil, dificil imposible exitosos

--- Codigo/Proyecto_Valle/Main.cpp
+++ Codigo/Proyecto_Valle/Main.cpp
@@ -3,28 +3,51 @@
 #include<string>
 #include<list>
 #include<chrono>
-//Variables globales para las dimensiones de la matriz
-//La Variable REAL_HEIGHT estará reservada para respaldo
+/**
+* Funcion principal para el algoritmo de estimacion, importante tener una base de datos con el siguiente formato
+* En la primera linea siempre debe tener las dimensiones de la matriz en [Ancho],[Alto]
+* Los argumentos, incluidas las cabeceras deben coincidir con las dimensiones especificadas
+* @author JoGEHrt
+*/
 int WIDTH = 0, HEIGHT = 0, REAL_HEIGHT = 0;
-//Métodos genéricos para transformar cadenas de caracteres en números enteros
+/**
+* Potencia de 10
+* @param n Grado de la potencia
+* @return 10^n
+*/
 int X10(const int n) {
 	int d = 1;
 	for (int i = 0; i < n; i++) d *= 10;
 	return d;
 }
+/**
+* Conversor de Cadenas tipo String a enteros
+* @param data Argumento a convertir
+* @return numero entero
+*/
 int toInt(const std::string data){
 	int r = 0;
 	for (int i = 0; i < data.length(); i++)
 		r += X10(data.length() - i -1) * (data[i] - 48);
 	return r;
 }
+/**
+* Es un ajuste temporal para las evaluaciones de presicion
+*/
 void set80p() {
 	HEIGHT = (int)((REAL_HEIGHT - 1) * 0.8f) + 1;
 }
+/**
+* Restaura las dimensiones reales
+*/
 void set100p() {
 	HEIGHT = REAL_HEIGHT;
 }
-//Carga en memoria de la base de datos de cualquier archivo que tenga el formato correcto
+/**
+* Carga los datos del archivo, debe espeficicarse su dirección relativa o absoluta, incluido la extension
+* @param id Dirección del archivo
+* @return Arreglo unidimensional de Cadenas
+*/
 std::string* getDataBase(const std::string id) {
 	std::ifstream archive;
 	archive.open(id, std::ios::in);
@@ -35,42 +58,55 @@ std::string* getDataBase(const std::string id) {
 	std::string line;
 	int begin = 0, index = 0;
 	std::getline(archive, line);
-	//Se extrae espeficamente la primera linea donde se almacena las dimensiones del arreglo
 	for (int i = 0; i < line.length(); i++) if (line[i] == ',') begin = i;
 	WIDTH = toInt(line.substr(0, begin));
 	REAL_HEIGHT = toInt(line.substr(begin + 1, line.length()));
 	HEIGHT = REAL_HEIGHT;
-	//Este se tratará como un arreglo unidimensional
 	std::string* data = new std::string[WIDTH*HEIGHT];
 	std::getline(archive, line);
 	begin = 0;
 	while (index < WIDTH*HEIGHT) {
 		for (int i = 0; i <= line.length(); i++) {
-			if (line[i] == ',' || line[i] == '\0') { //Cada que detecte una , o el fin de linea, de hará corte de la subsentiencia
-				data[index] = line.substr(begin, i - begin);//Almacenando en el correspondiente
+			if (line[i] == ',' || line[i] == '\0') {
+				data[index] = line.substr(begin, i - begin);
 				begin = i + 1;
 				index++;
 			}
 		}
-		//std::cout << std::endl;
 		std::getline(archive, line);
 		begin = 0;
 	}
 	std::cout << "Se han leido los datos correctamente...\n";
 	return data;
 }
+/**
+* Imprime todos los datos
+* @param matrix Base de datos
+*/
 void printData(std::string* matrix) {
-	//Este loop se usa como ejemplo para analizar la información como una tabla en dos dimensiones
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++)
 			std::cout << "|\t" << matrix[(i * WIDTH) + j] << "\t";
 		std::cout << "|\n";
 	}
 }
-//Este método está hecho para contar las veces que se repite un objetivo en mencionada categoría
+/**
+* Imprime la primera fila considerada como cabecera
+* @param matrix Base de datos
+*/
+void printHeader(std::string* matrix) {
+	for (int i = 0; i < WIDTH; i++)
+		std::cout << "|\t" << matrix[i] << "\t";
+	std::cout << "|\n";
+}
+/**
+* Veces que existe el objetivo en dicha categoría
+* @param matrix Base de datos
+* @param category Nombre en la cabecera
+* @return Numero de coincidencias
+*/
 int timesByCategory(std::string* matrix,const std::string category,const std::string name) {
 	int j = 0, count = 0; bool notFound = true;
-	//En el primer bucle se dedica a encontrar la columna
 	while (notFound)
 		if (!matrix[j].compare(category))
 			notFound = false;
@@ -78,20 +114,29 @@ int timesByCategory(std::string* matrix,const std::string category,const std::st
 			return count;
 		else
 			j++;
-	//Cuenta las veces que aparece en dicha columna
 	for (int i = 1; i < HEIGHT; i++)
 		if (!matrix[i * WIDTH + j].compare(name))
 			count++;
 	return count;
 }
-//Rutina complementaria para verificar la existencia de un elemento en la lista
+/**
+* Metodo complementario para indicar si se encuentra un elemento en la lista
+* @param lista coleccion de elementos
+* @param obj elemento en cuestion
+* @return True si se encuentra el elemento
+*/
 bool isOnList(const std::list<std::string> lista, const std::string obj){
 	for (std::string node : lista)
 		if (!node.compare(obj))
 			return true;
 	return false;
 }
-//Retorna una colección de los elementos en formato Lista
+/**
+* Metodo complementario para indicar si se encuentra un elemento en la lista
+* @param matrix Base de datos
+* @param category nombre de la categoria
+* @return Coleccion de elementos por categoria
+*/
 std::list<std::string> getListTypes(std::string* matrix, const std::string category) {
 	std::list<std::string> lista;
 	int j = 0; bool notFound = true;
@@ -103,15 +148,22 @@ std::list<std::string> getListTypes(std::string* matrix, const std::string categ
 		else
 			j++;
 	for (int i = 1; i < HEIGHT; i++)
-		if (!isOnList(lista, matrix[i * WIDTH + j])) //Si no está en la lista
-			lista.push_back(matrix[i * WIDTH + j]);  //Añade el elemento
-		
+		if (!isOnList(lista, matrix[i * WIDTH + j]))
+			lista.push_back(matrix[i * WIDTH + j]);
 	return lista;
 }
-//Metodo "Dado que" es una doble validación donde elementos de un par de columnas coincidan simultaneamente en sus objetivos
+/**
+* Metodo para encontrar coincidencias en el formato  A dado que B
+* @param matrix Base de Datos
+* @param ctg1 nombre de la categoria del primer elemento
+* @param nm1 nombre del primer objetivo
+* @param ctg2 nombre de la categoria del segundo elemento
+* @param nm2 nombre del segundo objetivo
+* @return Numero de coincidencias
+*/
 int dueTo(std::string* matrix, const std::string ctg1, const std::string nm1, const std::string ctg2, const std::string nm2) {
 	int j = 0, y = 0, coincidence = 0; bool nF1 = true, nF2 = true;
-	while (nF1 || nF2) {//Se localizan ambas columnas
+	while (nF1 || nF2) {
 		if (!matrix[j].compare(ctg1)) nF1 = false;
 		else if (nF1) j++;
 		if (!matrix[y].compare(ctg2)) nF2 = false;
@@ -124,30 +176,47 @@ int dueTo(std::string* matrix, const std::string ctg1, const std::string nm1, co
 			coincidence++;
 	return coincidence;
 }
-
-//Calcular la probabilidad
+/**
+* Obtención de la probabilidad de un evento contra todo el conjunto
+* @param matrix Base de Datos
+* @param category nombre de la categoria
+* @param name objetivo
+* @return Probabilidad entre 0 y 1
+*/
 float getProbability(std::string* matrix, const std::string category, const std::string name){
-	//Se multiplica las veces que aparece el objetivo dividido entre el total de elementos
 	return (float)(timesByCategory(matrix, category, name)) / (float)(HEIGHT - 1);
 }
+/**
+* Obtención de la probabilidad de un evento ocurra simultaneo a otro
+* @param matrix Base de Datos
+* @param ctg1 nombre de la categoria del primer elemento
+* @param nm1 nombre del primer objetivo
+* @param ctg2 nombre de la categoria del segundo elemento
+* @param nm2 nombre del segundo objetivo
+* @return Probabilidad entre 0 y 1
+*/
 float getProbability(std::string* matrix, const std::string ctg1, const std::string nm1, const std::string ctg2, const std::string nm2) {
-	//Se busca las coincidencias y se dividen entre las veces que aparece el primer argumento
 	return (float)(dueTo(matrix,ctg1,nm1,ctg2,nm2))/(float)(timesByCategory(matrix, ctg1, nm1));
 }
-
+/**
+* Metodo principal para obtener el principio de multiplicacion de probabilidad
+* @param matrix Base de Datos
+* @param conditions factores a considerar Categoria:Objetivo
+* @param question Categoria de la que se desea averiguar
+* @return Argumento mas probable
+*/
 std::string masProbable(std::string* matrix,const std::string conditions, const std::string question) {
 	int begin = 0, cat = 0; float fact = 1.0f, most = 0.0f; std::string answer = "No hay coincidencia"; bool alCat = true;
-	//Se obtiene la lista de la columna en cuestion
 	for (std::string possible : getListTypes(matrix, question)) {
 		fact = getProbability(matrix, question, possible); alCat = true; cat = 0; begin = 0;
 		std::cout << "Para " << possible << ": (" << timesByCategory(matrix, question, possible)<<"/"<<(HEIGHT-1)<<")";
-		for (int i = 0; i <= conditions.length(); i++) //Por cada condición presente
+		for (int i = 0; i <= conditions.length(); i++)
 			switch (conditions[i]) {
-			case  ',': //Caracteres que indican el fin de una condicion
+			case  ',':
 			case '\0':
 				std::cout << "*("<< dueTo(matrix, conditions.substr(begin, cat - begin), conditions.substr(cat + 1, i - (cat + 1)), question, possible);
 				std::cout << "/" << timesByCategory(matrix, conditions.substr(begin, cat - begin), conditions.substr(cat + 1, i - (cat + 1))) << ")";
-				fact *= getProbability(matrix, conditions.substr(begin,cat - begin), conditions.substr(cat + 1, i - (cat + 1)), question, possible); //Se acumula la probabilidad
+				fact *= getProbability(matrix, conditions.substr(begin,cat - begin), conditions.substr(cat + 1, i - (cat + 1)), question, possible);
 				begin = i + 1;
 				cat = i + 1;
 				alCat = true;
@@ -158,7 +227,7 @@ std::string masProbable(std::string* matrix,const std::string conditions, const 
 			default:
 				if (alCat)cat++;
 			}
-		if (fact > most) { //En caso de obtener una probabilidad mayor, se to´mará para ser devuelta
+		if (fact > most) {
 			possible.append("\tProbabilidad: ");
 			possible.append(std::to_string(fact));
 			answer = possible;
@@ -168,7 +237,13 @@ std::string masProbable(std::string* matrix,const std::string conditions, const 
 	}
 	return answer;
 }
-//Esencialmente es el mismo algoritmo, pero diseñado para pruebas de presición
+/**
+* Version del metodo que no imprime los resultados 
+* @param matrix Base de Datos
+* @param conditions factores a considerar Categoria:Objetivo
+* @param question Categoria de la que se desea averiguar
+* @return Argumento mas probable
+*/
 std::string presicionP(std::string* matrix, const std::string conditions, const std::string question) {
 	int begin = 0, cat = 0; float fact = 1.0f, most = 0.0f; std::string answer = "No hay coincidencia"; bool alCat = true;
 	for (std::string possible : getListTypes(matrix, question)) {
@@ -195,7 +270,13 @@ std::string presicionP(std::string* matrix, const std::string conditions, const 
 	}
 	return answer;
 }
-
+/**
+* Extrae las condiciones en el formato indicado
+* @param matrix Base de Datos
+* @param exception es el indice a la categoria a ignorar
+* @param fila es el nivel que recoje los objetivos
+* @return Condiciones en el formato Categoria:Objetivo,*
+*/
 std::string getConditions(std::string* matrix,const int exception, const int fila){
 	std::string args = "";
 	for(int i = 0; i < WIDTH ; i++)
@@ -208,7 +289,11 @@ std::string getConditions(std::string* matrix,const int exception, const int fil
 	args.resize(args.length() - 1);
 	return args;
 }
-
+/**
+* Metodo automatico para evaluar la presicion de la base de datos
+* @param matrix Base de Datos
+* @return Porcentaje de precision
+*/
 float testPresicion(std::string* matrix) {
 	set80p(); 
 	const int nTest = (REAL_HEIGHT - HEIGHT) * (WIDTH);
@@ -222,7 +307,10 @@ float testPresicion(std::string* matrix) {
 	set100p();
 	return ((float)hits / (float)nTest) * 100.0f;
 }
-
+/**
+* Implementacion con un menu de consola
+* @return Estado del programa
+*/
 int main() {
 	std::chrono::system_clock::time_point before, after;
 	std::chrono::duration<float, std::milli> time;
@@ -245,13 +333,14 @@ int main() {
 			break;
 		case '2':
 			std::cout << "Preguntar...\nInserte condiciones con el formato [Clasificacion]:[Objetivo],*\n";
+			printHeader(data);
 			std::cin >> id; std::cout << "Inserte la cuestion "; std::cin >> aux;
 			printData(data);
 			before = std::chrono::system_clock::now();
 			std::cout << "Resultado posible: " << masProbable(data,id,aux);
 			after = std::chrono::system_clock::now();
 			time = after - before;
-			std::cout << "Realizado en: " << time.count() << " milisegundos";
+			std::cout << "\nRealizado en: " << time.count() << " milisegundos";
 			std::cin >> aux;
 			break;
 		case '3':
